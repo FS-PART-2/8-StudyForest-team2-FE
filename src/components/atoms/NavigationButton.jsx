c0952f64e80b8eddafae6487ea8f00f2e0f76930
seongEun95 <dsa02070@naver.com>
2025-08-30 17:25:57 +0900

    feat: 네비게이션 버튼 컴포넌트 및 스타일 추가

---
+++ src/components/atoms/NavigationButton.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+import styles from '../../styles/components/atoms/NavigationButton.module.css';
+
+/**
+ * 네비게이션 버튼 컴포넌트
+ * @param {string} to 이동할 경로
+ * @param {React.ReactNode} children 내부 텍스트
+ * @returns
+ */
+export default function NavigationButton({ to, children }) {
+  return (
+    <Link to={to} className={styles.navigationButton}>
+      <span>{children}</span>
+      <img src="/assets/icons/right-arrow.svg" alt="right-arrow" />
+    </Link>
+  );
+}
