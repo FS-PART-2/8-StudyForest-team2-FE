e5b66a59ce7108189b0d1f1dde16cdc2ca4f2c1d
Aria Na <najuhyun.aria@gmail.com>
2025-09-11 08:58:11 +0900

    fix: main.jsx FocusPage 라우트 경로 수정
    - /focus에서 /focus/:id로 변경하여 스터디 ID 전달
    - StudyDetailPage와 HabitPage의 '오늘의 집중' 버튼과 일치

--- src/main.jsx
+++ src/main.jsx
@@ -35,7 +35,7 @@ export function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<StudyPage />} />
-            <Route path="/focus" element={<FocusPage />} />
+            <Route path="/focus/:id" element={<FocusPage />} />
             {/* <Route path="/test" element={<TestPage />} /> 프로덕션에서 제거 */}
             <Route path="/study/new" element={<StudyCreatePage />} />
             <Route path="/study/:id" element={<StudyDetailPage />} />
