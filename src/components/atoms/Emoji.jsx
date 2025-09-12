c987add6a2b67ad212112a784cdd84ae1d0e8bfa
seongEun95 <dsa02070@naver.com>
2025-09-12 15:59:19 +0900

    fix: 이모지 컴포넌트에서 이모지 표시 방식 수정

--- src/components/atoms/Emoji.jsx
+++ src/components/atoms/Emoji.jsx
@@ -19,24 +19,24 @@ export default function Emoji({
     onClick({ id, emoji, count });
   };
   // 16진수를 10진수로 변환
-  let emojiChar = ''; // 기본값으로 빈 문자열 설정
+  // let emojiChar = ''; // 기본값으로 빈 문자열 설정
 
-  // emoji 값이 유효한 문자열인지, 그리고 숫자로 변환 가능한지 확인
-  if (emoji && typeof emoji === 'string') {
-    const emojiDecimal = parseInt(emoji, 16);
+  // // emoji 값이 유효한 문자열인지, 그리고 숫자로 변환 가능한지 확인
+  // if (emoji && typeof emoji === 'string') {
+  //   const emojiDecimal = parseInt(emoji, 16);
 
-    // parseInt 결과가 유효한 숫자인지 확인
-    if (!isNaN(emojiDecimal)) {
-      emojiChar = String.fromCodePoint(emojiDecimal);
-    }
-  }
+  //   // parseInt 결과가 유효한 숫자인지 확인
+  //   if (!isNaN(emojiDecimal)) {
+  //     emojiChar = String.fromCodePoint(emojiDecimal);
+  //   }
+  // }
 
   return (
     <button
       className={`${styles.emojiContainer} ${styles[size]}`}
       onClick={handleClick}
     >
-      <span>{emojiChar}</span>
+      <span>{emoji}</span>
       <span>{count}</span>
     </button>
   );
